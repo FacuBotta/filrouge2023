be061f627af066886cdf3d99e92826ea35336794
FacuBotta <nuestampados@gmail.com>
2023-03-18 14:46:54 +0100

    new branch commit - burguer_menu updated

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,33 @@
-// burguer
+// burguer menu
 
-document.querySelector(".bars_menu").addEventListener("click", animateBars);
-var line1_bars = document.querySelector(".line1_bars_menu");
-var line2_bars = document.querySelector(".line2_bars_menu");
-var line3_bars = document.querySelector(".line3_bars_menu");
+let burguer_menu = document.getElementById('bars_menu');
+let li_nav_menu = document.querySelectorAll('nav.hidden_nav_menu li');
+let nav_menu = document.getElementById('nav_menu');
 
-function animateBars() {
-    line1_bars.classList.toggle("activeline1_bars_menu");
-    line2_bars.classList.toggle("activeline2_bars_menu");
-    line3_bars.classList.toggle("activeline3_bars_menu");
-}
+toogle_menu = () => {
+    document.querySelector('.line1_bars_menu').classList.toggle('activeline1_bars_menu');
+    document.querySelector('.line2_bars_menu').classList.toggle('activeline2_bars_menu');
+    document.querySelector('.line3_bars_menu').classList.toggle('activeline3_bars_menu');
 
-var burguer_menu = document.getElementById('bars_menu');
-let li_nav_menu = document.querySelectorAll(`div.container_nav li`);
+    if (nav_menu.className === 'hidden_nav_menu') {
+        nav_menu.classList.replace('hidden_nav_menu', 'active_nav_menu')
+    } else if (nav_menu.className === 'active_nav_menu') {
+        nav_menu.classList.replace('active_nav_menu', 'hidden_nav_menu')
+    }
 
-burguer_menu.addEventListener('click', () => {
-    let i = 0;
-    li_nav_menu.forEach(li_item => {
-        if (li_item.className === 'hidden_menu') {
-            li_item.classList.replace('hidden_menu', `active_menu_${i++}`);
-        } else if (li_item.className != `hidden_menu`) {
-            li_item.classList.replace(`active_menu_${i++}`, 'hidden_menu');
+    li_nav_menu.forEach((li_item, i) => {
+        if (li_item.className === 'hidden_menu_items') {
+            li_item.classList.replace('hidden_menu_items', `active_menu_item_${i++}`);
+        } else if (li_item.className != `hidden_menu_items`) {
+            li_item.classList.replace(`active_menu_item_${i++}`, 'hidden_menu_items');
         };
     });
-});
+}
+
+burguer_menu.addEventListener('click', toogle_menu);
+
+document.onclick = (e) => {
+    if (!burguer_menu.contains(e.target) && nav_menu.className != 'hidden_nav_menu') {
+        toogle_menu();
+    };
+};
