c50565ff6a80e73bc179ed6131e70108912c0f6c
FacuBotta <nuestampados@gmail.com>
2023-06-24 10:12:13 +0200

    conexion model update to google cloud

--- models/connect.php
+++ models/connect.php
@@ -1,5 +1,5 @@
 <?php
-$sessionDuration = 30 * 60;
+/* $sessionDuration = 30 * 60;
 session_set_cookie_params($sessionDuration);
 session_start();
 ini_set('display_errors', 1);
@@ -11,4 +11,17 @@ $_SESSION['last_activity'] = time();
 $DB_USER = "root";
 $DB_PASS = "test";
 $bdd = new PDO('mysql:host=db;dbname=acquaforte_bdd', $DB_USER, $DB_PASS,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => false));
+ */
 
+$servername = '34.155.36.200';
+$username = 'admin';
+$password = 'adminpass';
+$dbname = 'acquaforte_bdd';
+
+try {
+    $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password ,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+    echo 'Conexión exitosa'; // Esto es opcional, solo para verificar que la conexión se haya establecido correctamente
+} catch (PDOException $e) {
+    die('Error de conexión: ' . $e->getMessage());
+}
