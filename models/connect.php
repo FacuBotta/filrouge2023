6f43369a06b5e49e7908a308985252d9be67902f
FacuBotta <nuestampados@gmail.com>
2023-06-25 10:26:39 +0200

    connexion update

--- models/connect.php
+++ models/connect.php
@@ -23,11 +23,11 @@ error_reporting(E_ALL);
 $_SESSION['last_activity'] = time();
 
 
-$DB_HOST = $_ENV["BD_HOST"];
-$DB_USER = $_ENV["BD_USER"];
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
 $DB_PASSWORD = $_ENV["DB_PASSWORD"];
-$DB_NAME = $_ENV["BD_NAME"];
-$DB_PORT = $_ENV["BD_PORT"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
 
 try {
     $bdd = new PDO("mysql:host=$DB_HOST;port=$DB_PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
